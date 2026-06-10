61cb1f1a5187fe3653d83fabcfbae01e2dd278d3
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2026-06-10 20:26:33 +0530

    added next script

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,15 @@
 "use client"
 import Landing from '@/components/landing';
-
+import Script from 'next/script';
 
 export default function Home() {
   return (<>
           <Landing/>
-            <script
+            <Script
                 src="https://cloud.livekit.io/embed-popup.js"
                 data-lk-agent="CA_rXQnHbSdMyBx"
                 data-lk-color="#002CF2"
                 async
-          ></script>
+          ></Script>
   </>);
 }
